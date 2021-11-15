89053407388fc084fd04cb62e28e6ac6220187b7
thanh.nk <Thanh_nk@gdo.co.jp>
2021-11-15 17:46:11 +0700

    show todo list

---
+++ src/components/ToDo/ToDoItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    {{ todo.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["todo"]
+}
+</script>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <h1>Vue ToDo List</h1>
+    <div class="container">
+      <div class="header">
+      <p>Tasks: <span>{{todos.filter(todo => {return todo.done === false}).length}}</span></p>
+      <p>Done: <span>{{todos.filter(todo => {return todo.done === true}).length}}</span></p>
+      </div>
+      <div class="main">
+        <ul>
+          <li v-for="todo in todos">
+            <ToDoItem :todo="todo"></ToDoItem>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoItem from './ToDo/ToDoItem'
+export default {
+  name: "ToDoList",
+  components: {
+    'ToDoItem': ToDoItem
+  },
+  data() {
+  return {
+    todos: [{
+      id: 1,
+      title: 'Todo A',
+      project: 'Project A',
+      done: false,
+    }, {
+      id: 2,
+      title: 'Todo B',
+      project: 'Project B',
+      done: true,
+    }, {
+      id: 3,
+      title: 'Todo C',
+      project: 'Project C',
+      done: false,
+    }, {
+      id: 4,
+      title: 'Todo D',
+      project: 'Project D',
+      done: false,
+    }],
+  };
+}
+};
+</script>
+
+<style lang="scss">
+  @import '../assets/css/main.scss';
+</style>
